200fc8953562fad7e5e60d5af7531efed8f98b03
Etsegenet <114258837+alamerewetsegenet@users.noreply.github.com>
2023-07-11 14:46:45 -0700

    assignment_2

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     const data = dataModule.getAll();
-    res.render('home', {cars});
+    res.render('home', {data});
 });
 
 app.get('/detail', (req, res) => {
-    const name = req.query.name;
-    const item = dataModule.getItem(name);
+    const model = req.query.model;
+    const item = dataModule.getItem(model);
     res.render('detail', {item});
+    //console.log(model)
 });
 
 app.listen(3000, () => console.log('Listening on port 3000'));
