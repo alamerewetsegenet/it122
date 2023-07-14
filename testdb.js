aeaf430b6d6674c1d123c94bbfd4fdb0e3c2869a
Etsegenet <114258837+alamerewetsegenet@users.noreply.github.com>
2023-07-14 16:52:07 -0700

    assignment_s

--- testdb.js
+++ testdb.js
@@ -1,10 +1,10 @@
 'use strict'
 
-import { Car } from "./models/Car.js";
-
+import { Car } from "./models/Car.js"
 
 Car.find({}).lean()
-  .then((books) => {
-    console.log(cars);
-  })
-  .catch(err => next(err));
+.then((cars) => {
+  console.log(cars);
+})
+.catch(err => console.log(err));
+
