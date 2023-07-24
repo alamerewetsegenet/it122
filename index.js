3c93775309d6abf979863b0694d186cf0f57ac73
Etsegenet <114258837+alamerewetsegenet@users.noreply.github.com>
2023-07-24 16:45:06 -0700

    assignment_4

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ import express from 'express';
 const app = express();
 import { getAll, getItem } from "./data.js";
 
+//API
+import cors from 'cors';
+app.use('/api', cors()); // set Access-Control-Allow-Origin header for api route
 
+import { Car }  from "./models/Car.js";
+//end
 
 app.set('view engine', 'ejs');
 app.set('view options', { layout: './layouts/main.hbs'});
@@ -23,6 +28,32 @@ app.get('/delete', (req, res) => {
     res.render('delete', {item});
 });
 
+
+//API
+
+app.get('/api/cars', (req,res) => {
+    Car.find({}).lean()
+      .then((cars) => {
+        res.json(cars);})
+      .catch(err =>  {
+        res.status(500).send('Database Error occurred');
+      })
+});
+
+app.get('/api/cars/:make', (req,res) => {
+    Car.findOne({ make:req.params.make }).lean()
+        .then((cars) => {
+           res.json(cars);
+        })
+        .catch(err => {
+            res.status(500).send('Database Error occurred');
+        });
+});
+
+
+// fetch("/api/v1/cars")
+//     .then((response) => response.json())
+//     .then(results => console.log(results));
 
 
 app.listen(3000, () => console.log('Listening on port 3000'));
