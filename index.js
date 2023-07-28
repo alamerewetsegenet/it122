d5c379f14daca4c72aa2be5dd167965af6b6636d
Etsegenet <114258837+alamerewetsegenet@users.noreply.github.com>
2023-07-28 11:29:31 -0700

    assignment_4

--- index.js
+++ index.js
@@ -25,110 +25,51 @@ app.get('/detail', (req, res) => {
 
 //API
 
-app.get('/api/cars', (req, res, next) => {
-    Car.find({}, (err, items) => {
-     if (err) return next(err);
-     res.json(items);
-    });
-    });
-  
-  //get 1 car data
-  app.get('/api/car/:make', (req, res, next) => {
-    Car.findOne({'make': req.params.make}, (err, item) => {
-      if (err) return next(err);
-      if (item){
-      res.json(item);
-      }
-      else {
-        return res.status(500).send('Sorry this car does not exist');
-      }
-    }); 
-  });
-  
-  // delete 1 car
-  app.get('/api/delete/:make', (req, res, next) => { 
-      Countries.findOne({'make': req.params.make}, (err, result) => {
+//get all
+app.get('/api/cars', (req,res) => {
+    Car.find({}).lean()
+      .then((cars) => {
+        res.json(cars);})
+      .catch(err =>  {
+        res.status(500).send('Database Error occurred');
+      })
+});
+
+//get one 
+app.get('/api/car/:make', (req,res) => {
+    Car.findOne({ make:req.params.make }).lean()
+        .then((cars) => {
+           res.json(cars);
+        })
+        .catch(err => {
+            res.status(500).send('Database Error occurred');I
+        });
+});
+
+// delete one 
+app.get('/api/delete/:make', (req,res, next) => {
+    Car.deleteOne({"make":req.params.make }, (err, result) => {
         if (err) return next(err);
-        if (result){
-       Car.deleteOne({'make': req.params.make}, (err, item) => {
-        if (err) return next(err);
-        res.json(["successed, You have just deleted:  ", result]);  
-       });}
-      else{
-        return res.status(500).send('Sorry this car does not exist');
-      }
-    });  
-   });
-  
-    //add post route from the apiform
- app.post('/api/add/', (req, res, next) => {
-    var newItem = {'make': req.body.make, 'model':req.body.model, 'year': req.body.year, 'color': req.body.color, 'price': req.body.price };
-    Car.findOneAndUpdate({'make': req.body.make}, newItem, {upsert: true, new:true, useFindAndModify: false}, (err, result) => {
-        res.json(["successed, You have just Added/Updated:  ", result]);
+        res.json({"deleted": result});
     });
-  });
-//get all cars 
-// app.get('/api/cars', (req,res) => {
-//     Car.find({}).lean()
-//       .then((cars) => {
-//         res.json(cars);})
-//       .catch(err =>  {
-//         res.status(500).send('Database Error occurred');
-//       })
-// });
-
-// //get one car
-// app.get('/api/cars/:make', (req,res) => {
-//     Car.find({ make:req.params.make }).lean()
-//         .then((cars) => {
-//            res.json(cars);
-//         })
-//         .catch(err => {
-//             res.status(500).send('Sorry this car does not exist');
-//         });
-// });
-
-// //delete one car
-// app.get('/api/delete/:make', (req,res, next) => {
-//     Car.deleteOne({"item":req.params.make}, (err, result) => {
-//         if (err) return next(err);
-//         // return # of items deleted
-//         res.json("successed, you have deleted:", result);
-//     });
-// });
-
-// app.get('/api/delete/:name', (req, res, next) => { 
-//     Countries.findOne({'name': req.params.name}, (err, result) => {
-//       if (err) return next(err);
-//       if (result){
-//      Countries.deleteOne({'name': req.params.name}, (err, item) => {
-//       if (err) return next(err);
-//       res.json(["successed, You have just deleted:  ", result]);  
-//      });}
-//     else{
-//       return res.status(500).send('Sorry this country does not exist');
-//     }
-//   });  
-//  });
+});
+// add
 app.post('/api/add/', (req,res, next) => {
-    // find & update existing item, or add new 
-    if (!req.body._id) { // insert new document
+    if (!req.body.make) { 
         let car = new Car(req.body);
         car.save((err,newCar) => {
             if (err) return next(err);
-            res.json({updated: 0, _id: newCar._id});
+            res.json({updated: 0, make: newCar.make});
         });
-    } else { // update existing document
-        Car.updateOne({ _id: req.body._id}, {make:req.body.make, model: req.body.model, pubdate: req.body.pubdate }, (err, result) => {
+    } else { 
+        Car.updateOne({ make: req.body.make}, {model:req.body.model, year: req.body.year, color: req.body.color, price: req.body.price, pubdate: req.body.pubdate }, (err, result) => {
             if (err) return next(err);
-            res.json({updated: result.nModified, _id: req.body._id});
+            res.json({updated: result.nModified, make: req.body.make});
         });
     }
 });
 
-// fetch("/api/v1/cars")
-//     .then((response) => response.json())
-//     .then(results => console.log(results));
+
 
 
 app.listen(3000, () => console.log('Listening on port 3000'));
